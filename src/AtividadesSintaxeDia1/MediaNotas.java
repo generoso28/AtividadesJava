255ee9f01016e12e26d1d522c4780d2e3fbdee70
generoso28 <131410501+generoso28@users.noreply.github.com>
2024-02-26 21:39:50 -0300

    Atividades dia 1

--- src/AtividadesSintaxeDia1/MediaNotas.java
+++ src/AtividadesSintaxeDia1/MediaNotas.java
@@ -3,11 +3,6 @@ package AtividadesSintaxeDia1;
 import java.util.Scanner;
 
 public class MediaNotas {
-    //3. Solicite ao usuário que insira o número de alunos da turma.
-    //4. Crie um vetor (array) para armazenar as notas dos alunos, com o tamanho definido pelo número inserido pelo usuário.
-    //5. Utilize um loop for para solicitar as notas de cada aluno e armazená-las no vetor.
-    //6. Após coletar todas as notas, calcule a média das notas utilizando o vetor.
-    //7. Exiba a média das notas na tela.
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.print("Digite o número de alunos: ");
